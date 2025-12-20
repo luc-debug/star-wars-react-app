78ce9ab64c945e9b42a2407d2b12a7a2c8b7cdb1
Luca Berres <luca.berres@gmx.de>
2025-12-20 15:25:16 +0100

    feat: map section titles to icons in AppSidebar component

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -4,11 +4,29 @@ import * as React from "react";
 
 import { Item, NavMain } from "@/components/nav-main";
 import { Sidebar, SidebarContent, SidebarRail } from "@/components/ui/sidebar";
+import { User, Globe, Film, Rocket, Car, Dna } from "lucide-react";
 
 export function AppSidebar({
   data,
   ...props
 }: React.ComponentProps<typeof Sidebar> & { data: Item[] }) {
+  data = data.map((section) => ({
+    ...section,
+    icon:
+      section.title === "People"
+        ? User
+        : section.title === "Planets"
+        ? Globe
+        : section.title === "Films"
+        ? Film
+        : section.title === "Starships"
+        ? Rocket
+        : section.title === "Vehicles"
+        ? Car
+        : section.title === "Species"
+        ? Dna
+        : section.icon,
+  }));
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarContent>
