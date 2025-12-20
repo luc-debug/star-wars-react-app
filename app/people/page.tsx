5ff34f94b9efce071aaf8fd0b5a83ac45d222834
Luca Berres <luca.berres@gmx.de>
2025-12-20 18:35:58 +0100

    feat: add Loading component and simplify PeoplePage by removing Suspense

--- app/people/page.tsx
+++ app/people/page.tsx
@@ -1,11 +1,5 @@
-import { LoadingGrid } from "@/components/loading-grid";
 import { PeopleGrid } from "@/features/peoples/components/people-grid";
-import { Suspense } from "react";
 
 export default function PeoplePage() {
-  return (
-    <Suspense fallback={<LoadingGrid />}>
-      <PeopleGrid />
-    </Suspense>
-  );
+  return <PeopleGrid />;
 }
